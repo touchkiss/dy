75077913cd1170e7d87298bc39b2e90d7fcf4d8a
touchkiss <touchkiss@foxmail.com>
2020-11-29 21:54:23 +0800

    全新的请求方式

--- src/main/java/com/douyin/aweme/v1/bean/request/MusicDetailRequest.java
+++ src/main/java/com/douyin/aweme/v1/bean/request/MusicDetailRequest.java
@@ -3,6 +3,9 @@ package com.douyin.aweme.v1.bean.request;
 import com.douyin.aweme.v1.bean.response.MusicDetailResponse;
 import lombok.Data;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created on 2020/11/28 23:33
  *
@@ -10,6 +13,10 @@ import lombok.Data;
  */
 @Data
 public class MusicDetailRequest extends BaseRequest<MusicDetailResponse>{
-    public MusicDetailRequest() {
+//    public MusicDetailRequest() {
+//    }
+
+    public MusicDetailRequest(Set<String> headers, List<String> params, String host, String uri, String method) {
+        super(headers, params, host, uri, method);
     }
 }
